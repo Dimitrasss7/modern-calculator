ebbaa7a5dfcfef6f599ff893a23ab2b83115fcf3
Dimitrasss7 <yakaklegenda@gmail.com>
2025-10-02 23:56:24 +0300

    Add src/components/Button.jsx

---
+++ src/components/Button.jsx
@@ -0,0 +1,14 @@
+import './Button.css'
+
+const Button = ({ text, onClick, className = '' }) => {
+  return (
+    <button 
+      className={`calc-button ${className}`}
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  )
+}
+
+export default Button
